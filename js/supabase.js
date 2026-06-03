f75b000db63160073afefc12cd6b45fe527e46ae
lnegrettivilla-oss <lnegrettivilla@gmail.com>
2026-06-02 20:23:26 -0400

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,17 @@
+const SUPABASE_URL = "https://ywbtyjnfgvrybyuxixsv.supabase.co";
+const SUPABASE_KEY = "sb_publishable_ToQc7aO0GkuPQEBaPK1PJA_yJLuaOhy";
+
+async function obtenerProductos() {
+
+    const respuesta = await fetch(
+        `${SUPABASE_URL}/rest/v1/productos?select=*`,
+        {
+            headers: {
+                apikey: SUPABASE_KEY,
+                Authorization: `Bearer ${SUPABASE_KEY}`
+            }
+        }
+    );
+
+    return await respuesta.json();
+}
